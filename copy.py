2c64c18a4a62f9ac4c5d3d9d0ec8ecc294909b7d
sahil-lalani <slalani304@gmail.com>
2023-11-25 13:16:04 -0500

    new site

--- copy.py
+++ copy.py
@@ -2,7 +2,8 @@ import streamlit as st
 
 text_to_copy = st.text_input("Hello, World!")
 
-hosted_html_file = "https://copy-button.streamlit.app/files/copy.html"
+hosted_html_file = "https://silver-pavia-51.tiiny.site/"
 iframe_url = f"{hosted_html_file}?copy={text_to_copy}"
 
-st.markdown(f'<iframe src="{iframe_url}"></iframe>', unsafe_allow_html=True)
+if st.markdown(f'<iframe src="{iframe_url}"></iframe>', unsafe_allow_html=True):
+    st.write("cool")
