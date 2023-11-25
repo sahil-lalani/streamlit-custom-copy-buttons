4bb4b02dc9188ae56ac43fa17d571f041a350836
sahil-lalani <slalani304@gmail.com>
2023-11-25 12:59:19 -0500

    new url

--- copy.py
+++ copy.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 text_to_copy = st.text_input("Hello, World!")
 
-hosted_html_file = "https://everydayswag.org/files/copy.html"
+hosted_html_file = "https://copy-button.streamlit.app/files/copy.html"
 iframe_url = f"{hosted_html_file}?copy={text_to_copy}"
 
 st.markdown(f'<iframe src="{iframe_url}"></iframe>', unsafe_allow_html=True)
